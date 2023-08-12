dce76efa33a86754355b9e2d7eca54b09d44611b
baotuan8821 <tuanbao8821@gmail.com>
2023-08-12 14:32:29 +0700

    traiding market giá trị ảo

--- src/components/tradingMarket/tradingMarket.tsx
+++ src/components/tradingMarket/tradingMarket.tsx
@@ -1,8 +1,40 @@
-interface TradingMarketProps {
-  tradingData: { price: number; quantity: number; time: Date }[];
+import React, { useState, useEffect } from 'react';
+
+interface TradingItem {
+  price: number;
+  quantity: number;
+  time: Date;
 }
 
-function TradingMarket({ tradingData }: TradingMarketProps) {
+const TradingMarket: React.FC = () => {
+  const [tradingData, setTradingData] = useState<TradingItem[]>([]);
+
+  useEffect(() => {
+    // Generate initial data
+    const initialData: TradingItem[] = [];
+    for (let i = 0; i < 5; i++) {
+      initialData.push({
+        price: Math.random() * (1000 - 10.0) + 10.0,
+        quantity: Math.random() * (100 - 1.0) + 1.0,
+        time: new Date(),
+      });
+    }
+    setTradingData(initialData);
+
+    // Update data every 5 seconds
+    const interval = setInterval(() => {
+      const currentTime = new Date();
+      const newItem: TradingItem = {
+        price: Math.random() * (1000 - 10.0) + 10.0,
+        quantity: Math.random() * (100 - 1.0) + 1.0,
+        time: currentTime,
+      };
+      setTradingData((prevData) => [...prevData.slice(-4), newItem]);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className='bg-gray-800 p-4'>
       <h2 className='text-xl font-semibold text-white text-center mb-4'>Thị trường giao dịch</h2>
@@ -18,8 +50,12 @@ function TradingMarket({ tradingData }: TradingMarketProps) {
           <tbody>
             {tradingData.map((item, index) => (
               <tr key={index}>
-                <td className='px-4 py-2 text-lg font-semibold text-white'>{item.price.toFixed(2)}</td>
-                <td className='px-4 py-2 text-lg font-semibold text-white'>{item.quantity.toFixed(2)}</td>
+                <td className='px-4 py-2 text-lg font-semibold' style={{ color: '#3DB07C' }}>
+                  {item.price.toFixed(2)}
+                </td>
+                <td className='px-4 py-2 text-lg font-semibold text-white'>
+                  {item.quantity.toFixed(2)}
+                </td>
                 <td className='px-4 py-2 text-lg font-semibold text-white'>
                   {item.time.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' })}
                 </td>
@@ -30,6 +66,6 @@ function TradingMarket({ tradingData }: TradingMarketProps) {
       </div>
     </div>
   );
-}
+};
 
 export default TradingMarket;
