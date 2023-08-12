5450383ee6a982b3ce838acb5e37b05c51a92b8b
baotuan8821 <tuanbao8821@gmail.com>
2023-08-12 13:56:08 +0700

    chart bỏ hàm faker sử dụng math

--- src/components/chart/chart.tsx
+++ src/components/chart/chart.tsx
@@ -1,82 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import { ComposedChart, Area, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import faker from 'faker';
 
-interface ChartProps {
-  setTradingData: { price: number; quantity: number; time: Date }[];
-}
-
-const Chart: React.FC<ChartProps> = ({ setTradingData }: ChartProps) => {
-  const [data, setData] = useState<{ name: Date; marketPrice: number; tokenVNSePrice: number; tradingValue: number }[]>(
-    [],
-  );
+const CombinedChartComponent: React.FC = () => {
+  const [data, setData] = useState<{ name: Date; marketPrice: number; tokenVNSePrice: number; tradingValue: number }[]>([]);
 
   useEffect(() => {
     const interval = setInterval(() => {
       const currentTime = new Date();
-      const newMarketPrice = faker.random.number({ min: 1.0, max: 10.0 });
-      const newTokenVNSePrice = faker.random.number({ min: 100.0, max: 80.0 });
-      const newTradingValue = faker.random.number({ min: 167.0, max: 175.0 }) / 100.0;
-
       const newData = [
         ...data.slice(Math.max(data.length - 8, 0)),
         {
           name: currentTime,
-          marketPrice: newMarketPrice,
-          tokenVNSePrice: newTokenVNSePrice,
-          tradingValue: newTradingValue,
+          marketPrice: Math.random() * (10.000 - 1.000) + 1.000,
+          tokenVNSePrice: Math.random() * (10.0 - 8.0) + 8.0, // Adjusted range for smaller values
+          tradingValue: Math.random() * (175.00 - 167.00) + 167.00,
         },
       ];
       setData(newData);
-
-      // Update trading data
-      setTradingData((prevData) => [
-        ...prevData.slice(Math.max(prevData.length - 4, 0)),
-        {
-          price: newMarketPrice,
-          quantity: newTokenVNSePrice,
-          time: currentTime,
-        },
-      ]);
     }, 1000); // Update every 10 minutes
 
     return () => clearInterval(interval);
-  }, [data, setTradingData]);
+  }, [data]);
 
   return (
-    <div
-      className='absolute w-full h-422.188 bg-gray-900 rounded-lg shadow-md left-0 top-0'
-      style={{ width: '800.781px' }}
-    >
-      <h2 className='text-xl font-semibold mb-2 text-white'>Coin Market Price</h2>
-      <div className='w-full h-40'>
-        <ResponsiveContainer width='140%' height='180%'>
+    <div className="coin-market-chart p-4 bg-gray-900 rounded-lg shadow-md">
+      <h2 className="text-xl font-semibold mb-2 text-white">Coin Market Price</h2>
+      <div className="w-full h-40">
+        <ResponsiveContainer width="100%" height="100%">
           <ComposedChart data={data}>
-            <CartesianGrid strokeDasharray='3 3' vertical={false} strokeOpacity={0.2} />
-            <XAxis
-              dataKey='name'
-              tickFormatter={(value: Date) => value.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' })}
-            />
-            <YAxis yAxisId='left' domain={['auto', 'auto']} hide />
+            <CartesianGrid strokeDasharray="3 3" vertical={false} strokeOpacity={0.2} />
+            <XAxis dataKey="name" tickFormatter={(value) => (value as Date).toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' })} />
+            <YAxis yAxisId="left" domain={['auto', 'auto']} hide />
             <YAxis
-              yAxisId='right'
-              orientation='right'
-              domain={[0, 10]}
+              yAxisId="right"
+              orientation="right"
+              domain={[167, 175]}
               label={{ value: 'Trading Value', angle: -90, position: 'insideRight' }}
-              tickFormatter={(value: number) => value.toFixed(2)}
+              tickFormatter={(value) => value.toFixed(2)}
             />
             <Tooltip />
-            <Legend verticalAlign='top' height={36} />
-            <Area
-              type='monotone'
-              dataKey='marketPrice'
-              stroke='#113962'
-              fill='#113962'
-              fillOpacity={0.6}
-              yAxisId='left'
-              animationDuration={500}
-            />
-            <Bar dataKey='tokenVNSePrice' barSize={10} fill='#14406E' yAxisId='right' animationDuration={500} />
+            <Legend verticalAlign="top" height={36} />
+            <Area type="monotone" dataKey="marketPrice" stroke="#113962" fill="#113962" fillOpacity={0.6} yAxisId="left" animationDuration={500} />
+            <Bar dataKey="tokenVNSePrice" barSize={10} fill="#14406E" yAxisId="right" animationDuration={500} />
           </ComposedChart>
         </ResponsiveContainer>
       </div>
@@ -84,4 +49,4 @@ const Chart: React.FC<ChartProps> = ({ setTradingData }: ChartProps) => {
   );
 };
 
-export default Chart;
+export default CombinedChartComponent;
