679d9022e234c3b2b2bf915fb2e5356922cf45fd
20521051 <20521051@gm.uit.edu.vn>
2023-08-13 19:34:02 +0700

    continue: fix getBalance

--- src/context/VNSeContext.tsx
+++ src/context/VNSeContext.tsx
@@ -1,17 +1,19 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { createContext, ReactNode, useState } from 'react';
 
 declare let window: any;
-
 interface VNSeContextData {
   checkIfWalletIsConnect: () => void;
   connectWallet: () => void;
+  // getBalance: () => void;
   currentAccount: string | null;
+  balance: string | null;
 }
 
 export const VNSeContext = createContext<VNSeContextData | undefined>(undefined);
 
 export const VNSeProvider = ({ children }: { children: ReactNode }) => {
   const [currentAccount, setCurrentAccount] = useState<string | null>(null);
+  const [balance, setBalance] = useState<string | null>(null);
 
   const checkIfWalletIsConnect = async () => {
     try {
@@ -21,9 +23,14 @@ export const VNSeProvider = ({ children }: { children: ReactNode }) => {
 
       if (accounts.length) {
         setCurrentAccount(accounts[0]);
+        const balanceWei = await window.ethereum.getBalance(currentAccount);
+        const balanceEther = window.ethereum.utils.fromWei(balanceWei, 'ether');
+        setBalance(balanceEther);
       } else {
         console.log('No accounts found');
       }
+      console.log(currentAccount);
+      console.log(balance);
     } catch (error) {
       console.log(error);
     }
@@ -36,16 +43,22 @@ export const VNSeProvider = ({ children }: { children: ReactNode }) => {
       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
 
       setCurrentAccount(accounts[0]);
+      const balanceWei = await window.ethereum.getBalance(currentAccount);
+      const balanceEther = window.ethereum.utils.fromWei(balanceWei, 'ether');
+      setBalance(balanceEther);
+      console.log(currentAccount);
+      console.log(balance);
     } catch (error) {
       console.log(error);
-      throw new Error('No ethereum object');
     }
   };
 
   const contextValue: VNSeContextData = {
     checkIfWalletIsConnect,
     connectWallet,
+    // getBalance,
     currentAccount,
+    balance,
   };
 
   return <VNSeContext.Provider value={contextValue}>{children}</VNSeContext.Provider>;
